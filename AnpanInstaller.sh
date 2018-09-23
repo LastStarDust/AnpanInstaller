054f7f6cb0f1b9dd67dc6fd2485ba949149fc795
LastStarDust <giorgio.pimpa+github@gmail.com>
2018-09-23 06:39:09 +0200

    Added CentOS 7 compatibility. But still need to test it.

--- AnpanInstaller.sh
+++ AnpanInstaller.sh
@@ -20,52 +20,70 @@
 set -e
 
 ROOTREP="n"
-COUCHREP="n"
 DIMREP="n"
 LEVBDIMREP="n"
 LCIOREP="n"
 CONTINUE="n"
+UBUNTU="n"
+CENTOS="n"
 
 # Check the Ubuntu release
 
-if [ "`lsb_release -rs`" != "18.04" ];
+if [ "`lsb_release -rs`" != "18.04" ] && [ ! -f "/etc/redhat-release"];
 then
-    echo "This installer is for Ubuntu 18.04 only!"
-    echo "You can make this script to run also on other versions of Ubuntu"
-    echo "by simply replacing the 18.04 string on line 7 with your Ubuntu"
+    echo "This installer is for Ubuntu 18.04 and CentOS 7 only!"
+    echo "You can get this script to run also on other versions of Ubuntu"
+    echo "by simply replacing the 18.04 string on line 32 with your Ubuntu"
     echo "version but be warned that other modifications may be needed."
     exit 1
-fi  
+fi
+
+if [ "`lsb_release -rs`" == "18.04" ];
+then
+    UBUNTU="y"
+elif [ -f "/etc/redhat-release"];
+then
+    CENTOS="y"
+else
+    echo "There is something wrong about OS detection."
+    echo "UBUNTU = $UBUNTU"
+    echo "CENTOS = $CENTOS"
+    exit 1
+fi
 
 #check if sudo has been used
 
 if [ "`whoami`" == "root" ];
 then
     echo "This installer is not intended be run as root or with sudo"
-    echo "This doesn't mean that there are no sudo-ed commands INSIDE the script."
+    echo "You will need to insert the user password AFTER the script has started."
     exit 1
 fi
 
-# check for selinux
-#
-#if [ "`grep -c "SELINUX=disabled" /etc/selinux/config`" != "1" ];
-#then
-#    echo "Selinux is active. This prevents Anpan from working properly."
-#    echo "This operation will need a reboot. Please relaunch the installer"
-#    echo "as soon as reboot is completed."
-#    echo -n "Do you want this installer to fix the problem? (y|n) : "
-#    read REP
-#    if [ "${REP}" == "y" ];
-#    then
-#	echo "SELINUX=disabled" > /etc/selinux/config
-#	echo "SELINUXTYPE=targeted" >> /etc/selinux/config
-#	#reboot
-#	exit 1
-#    else
-#	echo "Please deactivate Selinux and try this installer again"
-#	exit 1
-#    fi
-#fi
+if [ $CENTOS == "y" ];
+then
+    # check for selinux
+
+    if [ "`grep -c "SELINUX=disabled" /etc/selinux/config`" != "1" ];
+    then
+	echo "SElinux is active. This prevents Anpan from working properly."
+	echo "This operation will need a reboot. Please relaunch the installer"
+	echo "as soon as reboot is completed."
+	echo -n "Do you want this installer to fix the problem? (y|n) : "
+	read REP
+	if [ "${REP}" == "y" ];
+	then
+	    echo "SELINUX=disabled" > /etc/selinux/config
+	    echo "SELINUXTYPE=targeted" >> /etc/selinux/config
+	    echo "Please reboot your machine and restart this installer."
+	    #reboot
+	    exit 1
+	else
+	    echo "Please deactivate Selinux and try this installer again"
+	    exit 1
+	fi
+    fi
+fi
 
 # Check for ROOT
 if [ "$ROOTSYS" == "" ];
@@ -100,118 +118,163 @@ then
 fi
 
 # Check for DIM
-if [ ! -d "/usr/local/lib/dim" ];
-then
-    echo ""
-    echo ""
-    echo "Dim is an optional dependency of anpan (only for SDHcal compat)."
-    echo "It seems that it is not installed (looking for /usr/local/lib/dim)"
-    echo "The total compilation of the sources should take very little time"
-    echo -n "Do you want this installer to install it? (y|n) : "
-    read DIMREP
-    if [ "${DIMREP}" == "n" ];
-    then
-	echo -n "Do you want this installer to continue anyway? (y|n) : "
-	read CONTINUE
-	if [ "${CONTINUE}" == "n" ];
-	then
-	    exit 1
-	else
-	    CONTINUE = "" 
-	fi
-    elif [ "${DIMREP}" == "y" ];
-    then
-	echo -n "Set to install it (DIMREP=\"y\")"
-    else
-	echo "I didn't understand your answer. Sorry, try again."
-	exit 1
-    fi
-fi
+# For the time being it is not used by Anpan
+DIMREP="n"
+# if [ ! -d "/usr/local/lib/dim" ];
+# then
+#     echo ""
+#     echo ""
+#     echo "Dim is an optional dependency of anpan (only for SDHcal compat)."
+#     echo "It seems that it is not installed (looking for /usr/local/lib/dim)"
+#     echo "The total compilation of the sources should take very little time"
+#     echo -n "Do you want this installer to install it? (y|n) : "
+#     read DIMREP
+#     if [ "${DIMREP}" == "n" ];
+#     then
+# 	echo -n "Do you want this installer to continue anyway? (y|n) : "
+# 	read CONTINUE
+# 	if [ "${CONTINUE}" == "n" ];
+# 	then
+# 	    exit 1
+# 	else
+# 	    CONTINUE = "" 
+# 	fi
+#     elif [ "${DIMREP}" == "y" ];
+#     then
+# 	echo -n "Set to install it (DIMREP=\"y\")"
+#     else
+# 	echo "I didn't understand your answer. Sorry, try again."
+# 	exit 1
+#     fi
+# fi
 
 # Check for Liblevbdim
-if [ ! -f "/usr/local/lib/dim/liblevbdim.so" ];
-then
-    echo ""
-    echo ""
-    echo "Levbdim is an optional dependency of anpan (only for SDHcal compat)."
-    echo "It seems that it is not installed (looking for /usr/local/lib/dim/liblevbdim.so)"
-    echo "Be aware that dim is a dependency for levbdim."
-    echo "The total compilation of the sources should take very little time"
-    echo -n "Do you want this installer to install it? (y|n) : "
-    read LEVBDIMREP
-    if [ "${LEVBDIMREP}" == "n" ];
-    then
-	echo -n "Do you want this installer to continue anyway? (y|n) : "
-	read CONTINUE
-	if [ "${CONTINUE}" == "n" ];
-	then
-	    exit 1
-	else
-	    CONTINUE = "" 
-	fi
-    elif [ "${LEVBDIMREP}" == "y" ];
-    then
-	echo -n "Set to install it (DIMREP=\"y\")"
-    else
-	echo "I didn't understand your answer. Sorry, try again."
-	exit 1
-    fi
-fi
+# For the time being it is not used by Anpan
+LEVBDIMREP="n"
+# if [ ! -f "/usr/local/lib/dim/liblevbdim.so" ];
+# then
+#     echo ""
+#     echo ""
+#     echo "Levbdim is an optional dependency of anpan (only for SDHcal compat)."
+#     echo "It seems that it is not installed (looking for /usr/local/lib/dim/liblevbdim.so)"
+#     echo "Be aware that dim is a dependency for levbdim."
+#     echo "The total compilation of the sources should take very little time"
+#     echo -n "Do you want this installer to install it? (y|n) : "
+#     read LEVBDIMREP
+#     if [ "${LEVBDIMREP}" == "n" ];
+#     then
+# 	echo -n "Do you want this installer to continue anyway? (y|n) : "
+# 	read CONTINUE
+# 	if [ "${CONTINUE}" == "n" ];
+# 	then
+# 	    exit 1
+# 	else
+# 	    CONTINUE = "" 
+# 	fi
+#     elif [ "${LEVBDIMREP}" == "y" ];
+#     then
+# 	echo -n "Set to install it (DIMREP=\"y\")"
+#     else
+# 	echo "I didn't understand your answer. Sorry, try again."
+# 	exit 1
+#     fi
+# fi
 
 # Check for LCIO
-if [ ! -d "/opt/lcio" ];
-then
-    echo ""
-    echo ""
-    echo "LCIO is an optional dependency of anpan."
-    echo "It seems that it is not installed (looking for /opt/lcio)"
-    echo "The total compilation of the sources should some minutes"
-    echo -n "Do you want this installer to install it? (y|n) : "
-    read LCIOREP
-    if [ "${LCIOREP}" == "n" ];
-    then
-	echo -n "Do you want this installer to continue anyway? (y|n) : "
-	read CONTINUE
-	if [ "${CONTINUE}" == "n" ];
-	then
-	    exit 1
-	else
-	    CONTINUE = "" 
-	fi
-    elif [ "${LCIOREP}" == "y" ];
-    then
-	echo -n "Set to install it (DIMREP=\"y\")"
-    else
-	echo "I didn't understand your answer. Sorry, try again."
-	exit 1
-    fi
-fi
+# For the time being it is not used by Anpan
+LCIOREP="n"
+# if [ ! -d "/opt/lcio" ];
+# then
+#     echo ""
+#     echo ""
+#     echo "LCIO is an optional dependency of anpan."
+#     echo "It seems that it is not installed (looking for /opt/lcio)"
+#     echo "The total compilation of the sources should some minutes"
+#     echo -n "Do you want this installer to install it? (y|n) : "
+#     read LCIOREP
+#     if [ "${LCIOREP}" == "n" ];
+#     then
+# 	echo -n "Do you want this installer to continue anyway? (y|n) : "
+# 	read CONTINUE
+# 	if [ "${CONTINUE}" == "n" ];
+# 	then
+# 	    exit 1
+# 	else
+# 	    CONTINUE = "" 
+# 	fi
+#     elif [ "${LCIOREP}" == "y" ];
+#     then
+# 	echo -n "Set to install it (DIMREP=\"y\")"
+#     else
+# 	echo "I didn't understand your answer. Sorry, try again."
+# 	exit 1
+#     fi
+# fi
 
 echo ""
 echo "Moving to the HOME directory."
 cd
 
 #install mandatory dependencies for pyrame and anpan
-if [ ! -f /etc/apt/sources.list.d/apache_couchdb_bionic.list ];
+if [ $UBUNTU == "y" ];
 then
-    sudo apt-get install curl
-    echo "deb https://apache.bintray.com/couchdb-deb bionic main" \
-        | sudo tee -a /etc/apt/sources.list.d/apache_couchdb_bionic.list
-    curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc \
-        | sudo apt-key add -
+    if [ ! -f /etc/apt/sources.list.d/apache_couchdb_bionic.list ];
+    then
+	sudo apt-get install curl
+	echo "deb https://apache.bintray.com/couchdb-deb bionic main" \
+            | sudo tee -a /etc/apt/sources.list.d/apache_couchdb_bionic.list
+	curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc \
+            | sudo apt-key add -
+    fi
+    sudo apt update
+    sudo apt-get install build-essential python python-devel python-pip psmisc \
+	 git libsdl1.2-dev libsdl-ttf2.0-dev elog python-sphinx \
+	 libafterimage-dev flex libexpat1-dev liblua5.2-dev \
+	 libcurl4 python-progressbar apache2 r-base \
+	 couch-libmozjs185-1.0 python-requests libmotif-dev tcsh \
+	 libxt-dev curl libboost-dev libboost-system-dev \
+	 libboost-filesystem-dev libboost-thread-dev libjsoncpp-dev \
+	 libcurl4-gnutls-dev scons libmongoclient-dev libboost-regex-dev
+elif [ $CENTOS == "y" ];
+then
+    if [ ! -f /etc/yum.repos.d/couchdb.repo ];
+    then
+	sudo tee /etc/yum.repos.d/couchdb.repo << 'EOF'
+[bintray--apache-couchdb-rpm]
+name=bintray--apache-couchdb-rpm
+baseurl=http://apache.bintray.com/couchdb-rpm/el$releasever/$basearch/
+gpgcheck=0
+repo_gpgcheck=0
+enabled=1
+EOF
+    fi
+    sudo yum -y install epel-release
+    sudo yum -y update
+    sudo yum install make automake gcc gcc-c++ kernel-devel python python-dev \
+	 python-pip psmisc git SDL-devel SDL_ttf-devel elog python-sphinx \
+	 libAfterImage flex expat-devel lua-devel libcurl python-progressbar \
+	 R httpd python-requests motif-devel tcsh libXt-devel curl \
+	 curl-devel boost-devel boost-filesystem boost-system boost-thread \
+	 boost-regex jsoncpp-devel scons libmongo-client	 
 fi
-sudo apt update
-sudo apt-get install build-essential python python-dev python-pip psmisc \
-     git libsdl1.2-dev libsdl-ttf2.0-dev elog python-sphinx \
-     libafterimage-dev flex libexpat1-dev liblua5.2-dev \
-     libcurl4 python-progressbar apache2 couchdb r-base \
-     couch-libmozjs185-1.0 python-requests libmotif-dev tcsh \
-     libxt-dev curl libboost-dev libboost-system-dev \
-     libboost-filesystem-dev libboost-thread-dev libjsoncpp-dev \
-     libcurl4-gnutls-dev git scons libmongoclient-dev libboost-regex-dev
-pip install --upgrade pyserial notify2 argparse
+
+echo ""
+echo "Be sure NOT to create a administrator user for couchdb!"
+echo "You avoid creating an administrator user by just inserting" 
+echo "an EMPTY password."
+read -n1 -r -p "Press any key to continue..." key
+
+if [ $UBUNTU == "y" ];
+then
+    sudo apt-get install couchdb
+elif [ $CENTOS == "y" ];
+then
+    sudo yum install couchdb
+fi
+
+pip install --upgrade pyserial notify2 argparse couchdb
 # If you want to generate the documentation, install also:
-pip install --upgrade docutils Pygments 
+pip install --upgrade docutils Pygments
 
 #install root if necessary
 if [ "${ROOTREP}" == "y" ];
@@ -232,18 +295,39 @@ then
         mkdir -p ${HOME}/ROOT
         ROOTSYS=${HOME}/ROOT
     fi
-    sudo apt-get install build-essential git dpkg-dev cmake xutils-dev \
-	 binutils libx11-dev libxpm-dev libxft-dev libxext-dev \
-	 libssl-dev libpcre3-dev libglu1-mesa-dev libglew-dev \
-	 libmysqlclient-dev libfftw3-dev libcfitsio-dev libgraphviz-dev \
-	 libavahi-compat-libdnssd-dev libldap2-dev python-dev libxml2-dev \
-	 libkrb5-dev libgsl-dev libqt4-dev libmotif-dev libmotif-common \
-	 libblas-dev liblapack-dev xfstt xfsprogs t1-xfree86-nonfree \
-	 ttf-xfree86-nonfree ttf-xfree86-nonfree-syriac xfonts-75dpi \
-	 xfonts-100dpi libgif-dev libtiff-dev libjpeg-dev liblz4-dev \
-	 liblzma-dev libgl2ps-dev libpostgresql-ocaml-dev libsqlite3-dev \
-	 libpythia8-dev davix-dev srm-ifce-dev libtbb-dev python-numpy
-    cd
+    
+    if [ $UBUNTU == "y" ];
+    then
+	sudo apt-get install build-essential git dpkg-dev cmake xutils-dev \
+	     binutils libx11-dev libxpm-dev libxft-dev libxext-dev \
+	     libssl-dev libpcre3-dev libglu1-mesa-dev libglew-dev \
+	     libmysqlclient-dev libfftw3-dev libcfitsio-dev libgraphviz-dev \
+	     libavahi-compat-libdnssd-dev libldap2-dev python-dev libxml2-dev \
+	     libkrb5-dev libgsl-dev libqt4-dev libmotif-dev libmotif-common \
+	     libblas-dev liblapack-dev xfstt xfsprogs t1-xfree86-nonfree \
+	     ttf-xfree86-nonfree ttf-xfree86-nonfree-syriac xfonts-75dpi \
+	     xfonts-100dpi libgif-dev libtiff-dev libjpeg-dev liblz4-dev \
+	     liblzma-dev libgl2ps-dev libpostgresql-ocaml-dev libsqlite3-dev \
+	     libpythia8-dev davix-dev srm-ifce-dev libtbb-dev python-numpy
+	cd
+    elif [ $CENTOS == "y" ];
+    then
+	sudo yum install make automake gcc gcc-c++ kernel-devel git cmake \
+	     xorg-x11-util-macros binutils libX11-devel libXft-devel openssl-devel \
+	     pcre2-devel mesa-libGLU-devel glew-devel avahi-compat-libdns_sd-devel \
+	     mariadb-devel fftw-devel graphviz-devel openldap-dvel python-devel \
+	     libxml2-devel krb5-devel gsl-devel qt-devel motif-devel motif \
+	     blas-devel lapack-devel xfsprogs cabextract xorg-x11-font-utils \
+	     fontconfig xorg-x11-server-Xvfb xorg-x11-fonts-Type1 \
+	     xorg-x11-fonts-75dpi xorg-x11-fonts-100dpi dejavu-sans-fonts urw-fonts \
+	     giflib-devel libtiff-devel libjpeg-turbo-devel lz4-devel xz-devel \
+	     gl2ps-devel postgresql-devel libsqlite3x-devel pythia8-devel \
+	     davix-devel srm-ifce-devel tbb-devel python2-numpy
+	
+	wget https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+	sudo yum install msttcore-fonts-installer-2.6-1.noarch.rpm
+	rm -f msttcore-fonts-installer-2.6-1.noarch.rpm
+    fi
     mkdir -p $ROOTSYS/{sources,6-14-02,6-14-02-build}
     cd $ROOTSYS
     git clone http://github.com/root-project/root.git sources
@@ -257,6 +341,7 @@ then
 fi
 
 #install dim if necessary
+
 if [ "${DIMREP}" == "y" ];
 then
     echo ""
@@ -299,7 +384,8 @@ then
     cd
 fi
 
-#install lcio if necessary
+# install lcio if necessary
+# for the time being it is not used by Anpan
 if [ "${LCIOREP}" == "y" ];
 then
     echo ""
@@ -342,12 +428,14 @@ if [ -z "$PYRAME_DIR" ]; then
 fi
 
 # In Debian systems you might need to create links for lua.h and liblua.so
-sudo ln -sf /usr/lib/x86_64-linux-gnu/liblua5.2.so /usr/lib/liblua.so
-sudo ln -sf /usr/include/lua5.2/lua.h /usr/include/lua.h
-sudo ln -sf /usr/include/lua5.2/luaconf.h /usr/include/luaconf.h
-sudo ln -sf /usr/include/lua5.2/lualib.h /usr/include/lualib.h
-sudo ln -sf /usr/include/lua5.2/lauxlib.h /usr/include/lauxlib.h
-
+if [ $UBUNTU == "y" ];
+then
+    sudo ln -sf /usr/lib/x86_64-linux-gnu/liblua5.2.so /usr/lib/liblua.so
+    sudo ln -sf /usr/include/lua5.2/lua.h /usr/include/lua.h
+    sudo ln -sf /usr/include/lua5.2/luaconf.h /usr/include/luaconf.h
+    sudo ln -sf /usr/include/lua5.2/lualib.h /usr/include/lualib.h
+    sudo ln -sf /usr/include/lua5.2/lauxlib.h /usr/include/lauxlib.h
+fi
 # check for previous installs
 if [ -d "${PYRAME_DIR}/pyrame" ];
 then
@@ -372,15 +460,35 @@ cd docs
 sudo -E make install
 cd ..
 
-# install and enable apache2 
-sudo "${PYRAME_DIR}/pyrame/xhr/install_xhr_debian8_apache2.sh"
-sudo systemctl restart apache2
-sudo systemctl enable apache2
+# install and enable apache2
+if [ $UBUNTU == "y" ];
+then
+    sudo "${PYRAME_DIR}/pyrame/xhr/install_xhr_debian8_apache2.sh"
+    sudo systemctl restart apache2
+    sudo systemctl enable apache2
+elif  [ $CENTOS == "y" ];
+then
+    sudo "${PYRAME_DIR}/pyrame/xhr/install_xhr_centos7_apache2.sh"
+    sudo systemctl restart httpd
+    sudo systemctl enable httpd
+fi
 
 # The following command is equivalent to
 # echo 1 > sudo tee /proc/sys/net/ipv4/tcp_tw_recycle
 # echo 1 > sudo tee /proc/sys/net/ipv4/tcp_fin_timeout
-sudo cp -f "${PYRAME_DIR}/pyrame/launcher/99-pyrame.conf" /etc/sysctl.d/
+# tcp_tw_recycle is not available in ubuntu since kernel 4.11,
+# moreover this is  strictly  a  violation  of the TCP specification.
+# In Linux 2.2, the default value for tcp_fin_timeout was 180 seconds.
+# I assumed that if the OS is CentOS that machine will only be used
+# as a DAQ machine with limited internet capabilities and so I allow
+# for a quick recycling of TCP connections. This is at expense of a
+# stable internet connection
+if [ $CENTOS == "y" ];
+then
+    echo 1 > sudo tee /proc/sys/net/ipv4/tcp_tw_recycle
+    echo 1 > sudo tee /proc/sys/net/ipv4/tcp_fin_timeout
+    sudo cp -f "${PYRAME_DIR}/pyrame/launcher/99-pyrame.conf" /etc/sysctl.d/
+fi  
 
 # --------------------- ANPAN ---------------------
 
@@ -431,7 +539,6 @@ cd docs/documentation
 make
 sudo mkdir -p /opt/anpan/doc
 sudo make install
-rm -rf /tmp/anpan.tgz
 cd ../..
 
 echo "Post-configuration..."
@@ -451,7 +558,15 @@ sudo systemctl enable pyrame
 sudo systemctl restart pyrame
 
 sleep 2s
-sensible-browser http://localhost/phygui_rc &
+
+if [ $UBUNTU == "y" ];
+then
+    sensible-browser http://localhost/phygui_rc &
+elif  [ $CENTOS == "y" ];
+then
+    firefox http://localhost/phygui_rc &
+fi
+
 
 echo "Installation successfully completed! Thanks for using Anpan"
 echo "For any questions about this script please contact:"
